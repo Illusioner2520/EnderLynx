aeabeccdcdbe9a942f2b49d3ec953714d357d464
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2026-07-07 20:24:20 -0600

    change flatpak runtime version

--- forge.config.js
+++ forge.config.js
@@ -8,136 +8,137 @@ const isWindows = process.platform === 'win32';
 const isMac = process.platform === 'darwin';
 
 module.exports = {
-  packagerConfig: {
-    osxUniversal: {
-      x64ArchFiles: '**/*'
+    packagerConfig: {
+        osxUniversal: {
+            x64ArchFiles: '**/*'
+        },
+        asar: {
+            unpack: [
+                '**/node_modules/@img/**',
+                '**/node_modules/create-desktop-shortcuts/src/*.vbs',
+                '**/up.exe'
+            ]
+        },
+        icon: path.resolve(__dirname, 'resources/icons/icon'),
+        ignore: [
+            /^\/\.github($|\/)/,
+            /^\/installer($|\/)/,
+            /^\/updater($|\/)/,
+            /\.gitignore$/,
+            /\.devdbrc$/,
+            /credits\.txt$/,
+            /forge\.config\.js$/,
+            /package-lock\.json$/,
+            /README\.md$/,
+            /LICENSE$/
+        ]
     },
-    asar: {
-      unpack: [
-        '**/node_modules/@img/**',
-        '**/node_modules/create-desktop-shortcuts/src/*.vbs',
-        '**/up.exe'
-      ]
-    },
-    icon: path.resolve(__dirname, 'resources/icons/icon'),
-    ignore: [
-      /^\/\.github($|\/)/,
-      /^\/installer($|\/)/,
-      /^\/updater($|\/)/,
-      /\.gitignore$/,
-      /\.devdbrc$/,
-      /credits\.txt$/,
-      /forge\.config\.js$/,
-      /package-lock\.json$/,
-      /README\.md$/,
-      /LICENSE$/
-    ]
-  },
-  hooks: {
-    postPackage: async () => {
-      // Delete non-English locale files
-      const localesDir = path.join(__dirname, 'out', '*', 'locales');
+    hooks: {
+        postPackage: async () => {
+            // Delete non-English locale files
+            const localesDir = path.join(__dirname, 'out', '*', 'locales');
 
-      try {
-        require('glob').sync(localesDir).forEach(dir => {
-          if (fs.existsSync(dir)) {
-            fs.readdirSync(dir).forEach(file => {
-              if (file != "en-US.pak") {
-                fs.unlinkSync(path.join(dir, file));
-              }
-            });
-          }
-        });
-      } catch (error) {
-        console.warn('Could not delete locales:', error.message);
-      }
-    }
-  },
-  rebuildConfig: {},
-  makers: [
-    // Windows
-    ...(isWindows ? [{
-      name: '@electron-forge/maker-zip',
-      platforms: ['win32'],
-    }] : []),
+            try {
+                require('glob').sync(localesDir).forEach(dir => {
+                    if (fs.existsSync(dir)) {
+                        fs.readdirSync(dir).forEach(file => {
+                            if (file != "en-US.pak") {
+                                fs.unlinkSync(path.join(dir, file));
+                            }
+                        });
+                    }
+                });
+            } catch (error) {
+                console.warn('Could not delete locales:', error.message);
+            }
+        }
+    },
+    rebuildConfig: {},
+    makers: [
+        // Windows
+        ...(isWindows ? [{
+            name: '@electron-forge/maker-zip',
+            platforms: ['win32'],
+        }] : []),
 
-    // Linux
-    ...(isLinux ? [{
-      name: '@electron-forge/maker-deb',
-      config: {
-        options: {
-          name: 'enderlynx',
-          productName: "EnderLynx",
-          genericName: "Minecraft Launcher",
-          maintainer: 'Illusioner',
-          section: 'games',
-          icon: './resources/icons/icon.png',
-          categories: ['Game'],
-          mimeType: ['application/zip', 'application/x-zip-compressed']
-        }
-      },
-    },
-    {
-      name: '@electron-forge/maker-rpm',
-      config: {
-        options: {
-          name: 'EnderLynx',
-          productName: "EnderLynx",
-          genericName: "Minecraft Launcher",
-          license: "MIT",
-          icon: "./resources/icons/icon.png",
-          categories: ['Game'],
-          mimeType: ['application/zip', 'application/x-zip-compressed']
-        }
-      },
-    },
-    {
-      name: '@electron-forge/maker-flatpak',
-      config: {
-        options: {
-          id: "me.illusioner.enderlynx",
-          productName: "EnderLynx",
-          genericName: "Minecraft Launcher",
-          icon: "./resources/icons/icon.png",
-          categories: ['Game'],
-          mimeType: ['application/zip', 'application/x-zip-compressed']
-        }
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['linux'],
-    }] : []),
+        // Linux
+        ...(isLinux ? [{
+            name: '@electron-forge/maker-deb',
+            config: {
+                options: {
+                    name: 'enderlynx',
+                    productName: "EnderLynx",
+                    genericName: "Minecraft Launcher",
+                    maintainer: 'Illusioner',
+                    section: 'games',
+                    icon: './resources/icons/icon.png',
+                    categories: ['Game'],
+                    mimeType: ['application/zip', 'application/x-zip-compressed']
+                }
+            },
+        },
+        {
+            name: '@electron-forge/maker-rpm',
+            config: {
+                options: {
+                    name: 'EnderLynx',
+                    productName: "EnderLynx",
+                    genericName: "Minecraft Launcher",
+                    license: "MIT",
+                    icon: "./resources/icons/icon.png",
+                    categories: ['Game'],
+                    mimeType: ['application/zip', 'application/x-zip-compressed']
+                }
+            },
+        },
+        {
+            name: '@electron-forge/maker-flatpak',
+            config: {
+                options: {
+                    id: "me.illusioner.enderlynx",
+                    productName: "EnderLynx",
+                    genericName: "Minecraft Launcher",
+                    icon: "./resources/icons/icon.png",
+                    categories: ['Game'],
+                    mimeType: ['application/zip', 'application/x-zip-compressed'],
+                    runtimeVersion: '24.08'
+                }
+            },
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['linux'],
+        }] : []),
 
-    // macOS
-    ...(isMac ? [{
-      name: '@electron-forge/maker-dmg',
-      config: {
-        name: 'EnderLynx',
-        format: 'ULFO',
-        icon: "./resources/icons/icon.icns"
-      },
-    },
-    {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    }] : [])
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    // Fuses are used to enable/disable various Electron functionality
-    // at package time, before code signing the application
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
+        // macOS
+        ...(isMac ? [{
+            name: '@electron-forge/maker-dmg',
+            config: {
+                name: 'EnderLynx',
+                format: 'ULFO',
+                icon: "./resources/icons/icon.icns"
+            },
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin'],
+        }] : [])
+    ],
+    plugins: [
+        {
+            name: '@electron-forge/plugin-auto-unpack-natives',
+            config: {},
+        },
+        // Fuses are used to enable/disable various Electron functionality
+        // at package time, before code signing the application
+        new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+        }),
+    ],
 };
