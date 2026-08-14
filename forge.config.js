e0589bc5b00713e0ae5ef857bea17b8791298b38
Illusioner2520 <96849357+Illusioner2520@users.noreply.github.com>
2026-08-13 21:31:17 -0600

    finally fix everything (hopefully)

--- forge.config.js
+++ forge.config.js
@@ -53,7 +53,9 @@ module.exports = {
             }
         }
     },
-    rebuildConfig: {},
+    rebuildConfig: {
+        onlyModules: []
+    },
     makers: [
         // Windows
         ...(isWindows ? [{
